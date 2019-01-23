24969df97d1bb1991fed4857947f51f75cdc17ea
TravisBledsoe <44755593+TravisBledsoe@users.noreply.github.com>
2019-01-23 08:36:37 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,24 +1,3 @@
-# The user needs to be able to input letter guesses.
-# A limit should also be set on how many guesses they can use.
-# This means you’ll need a way to grab a word to use for guessing.
-#  (This can be grabbed from a pre-made list. No need to get too fancy.)
-#  You will also need functions to check if the user has actually inputted
-#  a single letter, to check if the inputted letter is in the hidden word
-#  (and if it is, how many times it appears), to print letters, and a counter
-#  variable to limit guesses.
-# Concepts to keep in mind:
-
-# Random
-# Variables
-# Boolean
-# Input and Output
-# Integer
-# Char
-# String
-# Length
-# Print
-# from random import choice
-
 from random import choice
 
 # Grab a word for guessing from a pre-made list
